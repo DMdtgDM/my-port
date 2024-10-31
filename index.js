3f1467a1f040abe97796c3f60aa3648eef9bce2b
DMdtgDM <dmdtgdm@gmail.com>
2024-10-31 06:42:49 -0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// const arrow1 = document.querySelector(".arrow1");
+// const arrow2 = document.querySelector(".arrow2");
+
+// function toggle(arrow) {
+//     if (arrow.classList.contains("remove")) {
+//         arrow.classList.remove("remove");
+//         arrow.classList.add("arrowsOpe");
+//     } else {
+//         arrow.classList.add("remove");
+//         arrow.classList.remove("arrowsOpe");
+//     }
+// }
+
+// setInterval( () => {
+//     toggle(arrow1);
+//     toggle(arrow2)
+// }, 1000)
+
+
